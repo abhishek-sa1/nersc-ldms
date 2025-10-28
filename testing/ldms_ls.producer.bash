2edcacb6d391cbf91ff42484a10c85f36ff9e9e0
Abhishek S A <abhishek.sa3@dell.com>
2025-10-28 15:29:35 +0530

    ldms telemetry
    Signed-off-by: abhishek-sa1 <abhishek.sa3@dell.com>

--- testing/ldms_ls.producer.bash
+++ testing/ldms_ls.producer.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/ovis-ldms/sbin/ldms_ls -x sock -h localhost -p 6002  -l -a ovis -A conf=$(pwd)/ldmsauth.conf
+/opt/ovis-ldms/sbin/ldms_ls -x sock -h localhost -p 10001  -l -a ovis -A conf=$(pwd)/ldmsauth.conf
--- testing/ldmsd_producer.bash
+++ testing/ldmsd_producer.bash
@@ -2,4 +2,4 @@
 
 chmod 400 ldmsauth.conf 
 chmod 400 ldmsd_agg.conf
-/opt/ovis-ldms/sbin/ldmsd -x sock:6002 -c ldmsd_producer.conf -a ovis -A conf=$(pwd)/ldmsauth.conf -v DEBUG
+/opt/ovis-ldms/sbin/ldmsd -x sock:10001 -c ldmsd_producer.conf -a ovis -A conf=$(pwd)/ldmsauth.conf -v DEBUG
